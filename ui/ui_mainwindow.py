98b302d827f459ce7b17e497d6fe08acf0fe224f
Trild98 <vani_98@bk.ru>
2025-06-24 11:47:49 +1000

    Commit #8 struct 0.1.1

--- ui/ui_mainwindow.py
+++ ui/ui_mainwindow.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QHeaderView,
-    QMainWindow, QMenuBar, QSizePolicy, QStatusBar,
-    QTabWidget, QTreeView, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
+    QHeaderView, QLineEdit, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTableView,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -38,10 +39,62 @@ class Ui_MainWindow(object):
         self.widget_2.setMaximumSize(QSize(200, 16777215))
         self.horizontalLayout = QHBoxLayout(self.widget_2)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.treeView = QTreeView(self.widget_2)
-        self.treeView.setObjectName(u"treeView")
+        self.frame = QFrame(self.widget_2)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"background-color: white;")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.widget_4 = QWidget(self.frame)
+        self.widget_4.setObjectName(u"widget_4")
+        self.widget_4.setMinimumSize(QSize(0, 400))
+        self.verticalLayout_2 = QVBoxLayout(self.widget_4)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.findText = QLineEdit(self.widget_4)
+        self.findText.setObjectName(u"findText")
 
-        self.horizontalLayout.addWidget(self.treeView)
+        self.verticalLayout_2.addWidget(self.findText)
+
+        self.findButton = QPushButton(self.widget_4)
+        self.findButton.setObjectName(u"findButton")
+
+        self.verticalLayout_2.addWidget(self.findButton)
+
+        self.widget_6 = QWidget(self.widget_4)
+        self.widget_6.setObjectName(u"widget_6")
+
+        self.verticalLayout_2.addWidget(self.widget_6)
+
+
+        self.verticalLayout.addWidget(self.widget_4)
+
+        self.widget_5 = QWidget(self.frame)
+        self.widget_5.setObjectName(u"widget_5")
+        self.widget_5.setMinimumSize(QSize(0, 100))
+        self.widget_5.setMaximumSize(QSize(16777215, 100))
+        self.verticalLayout_3 = QVBoxLayout(self.widget_5)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.addButton = QPushButton(self.widget_5)
+        self.addButton.setObjectName(u"addButton")
+
+        self.verticalLayout_3.addWidget(self.addButton)
+
+        self.editButton = QPushButton(self.widget_5)
+        self.editButton.setObjectName(u"editButton")
+
+        self.verticalLayout_3.addWidget(self.editButton)
+
+        self.deleteButton = QPushButton(self.widget_5)
+        self.deleteButton.setObjectName(u"deleteButton")
+
+        self.verticalLayout_3.addWidget(self.deleteButton)
+
+
+        self.verticalLayout.addWidget(self.widget_5)
+
+
+        self.horizontalLayout.addWidget(self.frame)
 
 
         self.gridLayout_2.addWidget(self.widget_2, 0, 0, 1, 1)
@@ -50,28 +103,16 @@ class Ui_MainWindow(object):
         self.widget_3.setObjectName(u"widget_3")
         self.gridLayout_3 = QGridLayout(self.widget_3)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.tabWidget = QTabWidget(self.widget_3)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.tabWidget.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.tabWidget.addTab(self.tab_2, "")
+        self.tableView = QTableView(self.widget_3)
+        self.tableView.setObjectName(u"tableView")
 
-        self.gridLayout_3.addWidget(self.tabWidget, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.tableView, 0, 0, 1, 1)
 
 
         self.gridLayout_2.addWidget(self.widget_3, 0, 1, 1, 1)
 
 
-        self.gridLayout.addWidget(self.widget, 1, 0, 1, 1)
-
-        self.widget_4 = QWidget(self.centralwidget)
-        self.widget_4.setObjectName(u"widget_4")
-        self.widget_4.setMinimumSize(QSize(0, 50))
-
-        self.gridLayout.addWidget(self.widget_4, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
@@ -79,7 +120,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 993, 33))
+        self.menubar.setGeometry(QRect(0, 0, 993, 21))
         MainWindow.setMenuBar(self.menubar)
 
         self.retranslateUi(MainWindow)
@@ -89,7 +130,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
+        self.findButton.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0439\u0442\u0438", None))
+        self.addButton.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c", None))
+        self.editButton.setText(QCoreApplication.translate("MainWindow", u"\u0418\u0437\u043c\u0435\u043d\u0438\u0442\u044c", None))
+        self.deleteButton.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
     # retranslateUi
 
